d0578ef6d41de20a030c140bfe263d92fc103f64
isnowrain <Admin@Admin-PC.(none)>
2012-02-03 20:36:42 +0800

    ugh

---
+++ Examples/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CoreFoundation;
+using CFManzana;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CFManzana by iSn0wra1n");
+
+            iDevice dev = new iDevice();
+
+            Console.WriteLine(dev.CopyValue("Device Activation State: " + "ActivationState"));
+            
+        }
+    }
+}
